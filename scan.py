ccdd9f30bbb5dcaadc9552ec9080fac826c9d7fd
Tommy Smith <tls.tommy11@gmail.com>
2019-12-06 07:46:36 -0600

    commit

--- scan.py
+++ scan.py
@@ -1,6 +1,21 @@
 from getmac import getmac
+import socket
+
 
 validAddresses = []
+def getHostIp():
+    hostname = socket.gethostname()
+    hostIPAdress = socket.gethostbyname(hostname)
+    
+    print("\nHost Name: "+hostname)
+    print("Host IP: "+hostIPAdress)
+    print("\n")
+    
+    ipList = hostIPAdress.rpartition(".")
+
+    ipPrefix = ipList[0] + "."
+    
+    return ipPrefix
 
 def scan(firs,las):
     address = firs + las
@@ -12,12 +27,14 @@ def scan(firs,las):
             "IP": IP,
             "MAC": macAddress
         }
-        print("received",IP)
+        print("Received",IP)
         validAddresses.append(addressDict)
+    
 
 print("Starting Scan.\nThis will take a while....")
+prefix = getHostIp()
 for i in range(1,256):
-    scan("192.168.86.",str(i))
+    scan(prefix,str(i))
 print("\nDone!")
 print(validAddresses)
 # Changing the port used for updating ARP table (UDP packet)
