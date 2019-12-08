a07b18497091ec6f2239c398fa81cccd28e12ca0
lulamae12 <tls.tommy11@gmail.com>
2019-12-08 15:56:30 -0600

    comm

--- classScan.py
+++ classScan.py
@@ -1,8 +1,64 @@
 from getmac import getmac
-import os,time
+import os,time,sys,json,socket
 from urllib.request import urlopen
+validAddresses = {}
+validAddresses["Devices"] = []
+class DecodeDevices():
+    def __init__(self):
+        print("decoding File")
+        self.deviceJsonData = self.loadDict("devices.json")
+        self.macAddrData = self.loadDict("macAddreses.json")
+        self.getMacVendorSeg()
 
-validAddresses = []
+
+    def getMacVendorSeg(self):#gets vendor segment and rewrite file
+        devs = {}
+        devs["Devices"] = []
+        
+        
+        for item in self.deviceJsonData["Devices"]:
+            macAddress = item["MAC"]
+            ip = item["IP"]
+            print("mac addr",macAddress)
+            vendorID = macAddress.split(":",3)
+            
+            vendorID = vendorID[0].upper() + vendorID[1].upper() + vendorID[2].upper()
+            
+            print(vendorID)
+            
+            for address in self.macAddrData["Macs"]:
+                if address["address"] == vendorID:
+                    addr = address["address"]
+                    vend = address["vendor"]
+                    hostname = socket.gethostbyname_ex(ip)
+                  
+                    print(hostname)
+                              
+
+
+
+                    addressDict = {
+                        "IP": ip,
+                        "MAC": macAddress,
+                        "Vendor":vend,
+                        "Hostname":hostname
+                    }
+                
+                    devs["Devices"].append(addressDict)
+
+        with open("deviceData.json","w") as outFile:
+            json.dump(devs,outFile,indent=4)
+        outFile.close()
+
+
+    def loadDict(self,file):
+        with open(file) as json_file:
+            data = json.load(json_file)
+        return data
+    
+   
+
+
 
 class UpdateMacs():
     def __init__(self):
@@ -22,18 +78,17 @@ class UpdateMacs():
 
 
     def saveDict(self):
-        with open("ipMacAdds.txt","w") as outfile:
-            for item in validAddresses:
-                print(item)
-                outfile.write(item)
-                outfile.write("\n")
-        outfile.close()
+        
+        with open("devices.json","w") as outFile:
+            json.dump(validAddresses,outFile,indent=4)
+        outFile.close()
 
     def scanForMac(self,last):
         
         
 
-
+        data = {}
+        data["IP info"] = []
         os.system('cls')
         address = str(self.ipPrefix) + str(last)
         IP = address
@@ -50,14 +105,15 @@ class UpdateMacs():
                 "MAC": macAddress
             }
             print("Received",IP)
-            validAddresses.append(addressDict)
-        
+            validAddresses["Devices"].append(addressDict)
+            
+
 
     def iterThroughIps(self,rangeMin,rangeMax ):
         for i in range(int(rangeMin),int((rangeMax-1))):
             os.system('cls')
             self.scanForMac(i)
-        self.saveDict
+        self.saveDict()
 
 
 class DownloadMacs():
@@ -68,35 +124,55 @@ class DownloadMacs():
 
 
     def downloadSan(self):
-        lines = []
+        data = {}
+        data["Macs"] = []
         dataFile = urlopen(self.macSanURL)
         lineCount = 0
+        skipCount = 0 
         for line in dataFile:
             line = line.decode("utf-8")
             line=line.replace("\t",":");line=line.replace("\n","")
             
+            print(line)
+            try:
+                mac,vendor = line.split(":")
+            except:
+                skipCount = skipCount + 1
+                pass
 
-            
             print(line)
             lineCount = lineCount + 1
-            lines.append(line)
+            data["Macs"].append({
+                "address":mac,
+                "vendor":vendor
+                })
+            #print(lines)
         
-        with open("macAddreses.txt","w") as outFile:
-            for line in lines:
-                outFile.write(line)
-                outFile.write("\n")
+        with open("macAddreses.json","w") as outFile:
+            json.dump(data,outFile,indent=4)
 
         outFile.close
         print("Downloaded: ",lineCount," lines")
 
+        with open('macAddreses.json') as json_file:
+            data = json.load(json_file)
+        for p in data['Macs']:
+            print('Address: ' + p['address'])
+            print('Vendor: ' + p['vendor'])
+            
+            print('')
+        print("Downloaded: ",lineCount," lines")
+        print("skipped: ",skipCount," lines")
 
 
 
 
-DownloadMacs = DownloadMacs()
+#DownloadMacs = DownloadMacs()
 
-time.sleep(3)
+#time.sleep(3)
 os.system('cls')
 updateQuest = input("Scan Network? : ")
 if "y" in updateQuest:
     update = UpdateMacs()
+
+DecodeDevices()
